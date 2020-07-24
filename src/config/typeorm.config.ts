23e0477c7172b8b05fe784c3ba481b3448e93e7c
suvash23 <suvash.cse.duet@gmail.com>
2020-07-25 03:05:17 +0600

    CRUD opertation - config file added

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,11 @@
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+export const typeOrmConfig: TypeOrmModuleOptions = {
+  type: 'mysql',
+  host: 'localhost',
+  port: 3306,
+  username: 'root',
+  password: '',
+  database: 'taskmanagement',
+  entities: [__dirname + '/../**/*.entity.js'],
+  synchronize: true,
+}
---
+++ src/tasks/task.repository.ts
@@ -0,0 +1,33 @@
+import { Repository, EntityRepository } from "typeorm";
+import { Task } from "./task.entity";
+import { CreateTaskDto } from "./dto/create-task.dto";
+import { TaskStatus } from "./task-status.enum";
+import { GetTasksFilterDto } from "./dto/get-tasks-filter.dto";
+
+@EntityRepository(Task)
+export class TaskRepository extends Repository<Task> {
+  async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    const task = new Task();
+    const { title, description } = createTaskDto;
+    task.title = title;
+    task.description = description;
+    task.status = TaskStatus.OPEN;
+    await task.save();
+    return task;
+  }
+
+  async getTasks(taskFilterDto: GetTasksFilterDto): Promise<Task[]> {
+    const { status, search } = taskFilterDto;
+    const query = this.createQueryBuilder('task');
+
+    if (status) {
+      query.andWhere('task.status = :status', { status });
+    }
+
+    if (search) {
+      query.andWhere('task.title LIKE :search OR task.description LIKE :search', { search: `%${search}%` });
+    }
+    const tasks = await query.getMany();
+    return tasks;
+  }
+}
